452586af7b388a9daf13fa9924bbf6f7d1b57de7
DESKTOP-E93NKT3\hi-sinchon <dmstnr738@gmail.com>
2024-06-26 18:03:10 +0900

    d

--- src/main/java/com/ohgiraffers/AssignmentApplication.java
+++ src/main/java/com/ohgiraffers/AssignmentApplication.java
@@ -1,4 +1,4 @@
-package com.ohgiraffers.assignment;
+package com.ohgiraffers;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/main/java/com/ohgiraffers/assignment/SwaggerTestController.java
+++ src/main/java/com/ohgiraffers/assignment/SwaggerTestController.java
@@ -1,7 +1,6 @@
 package com.ohgiraffers.assignment;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
-import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
